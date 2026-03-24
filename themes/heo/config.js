51af5dbd6ed12dd564882b590a580db1c29c42a1
承潜 <2972013548@qq.com>
2026-03-24 18:03:55 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://github.com/MoChengqian' },
-   // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到莫承潜的博客', url: 'https://github.com/MoChengqian' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,43 +17,40 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  //HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  //HEO_HERO_TITLE_4: '新版上线',
-  //HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  //HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '莫承潜',
+  HEO_HERO_TITLE_2: '个人博客',
+  HEO_HERO_TITLE_3: '记录学习、设计与生活',
+  HEO_HERO_TITLE_4: '欢迎来到',
+  HEO_HERO_TITLE_5: 'MoChengqian',
+  HEO_HERO_TITLE_LINK: 'https://github.com/MoChengqian',
+
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '随笔', url: '/tag/随笔' },
+  HEO_HERO_CATEGORY_2: { title: '技术', url: '/tag/技术' },
+  HEO_HERO_CATEGORY_3: { title: '生活', url: '/tag/生活' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://img.notionusercontent.com/s3/prod-files-secure%2Fd89f2367-b54a-81cd-9c59-00032f2f4f1e%2F20106dfd-daae-4c1c-9da9-0aadb842daaa%2FIMG_3068.jpeg/size/w=2000?exp=1774431640&sig=3duEL5MNEWXn_tKaDJKusi3s6LBHZr_GOkyXVofDmCY&id=8a5f2367-b54a-83fe-bc15-81705a00ada1&table=block&userId=2b6d872b-594c-81e1-ab68-00024a8a1d96', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: '/images/hero-mountain.png', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是莫承潜!',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '你好！我是莫承潜',
+    '欢迎来到我的博客',
+    '记录学习、设计与生活'
   ],
 
   // 个人资料底部按钮
-  //HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/MoChengqian',
-  //HEO_INFO_CARD_ICON2: 'fab fa-github',
-  //HEO_INFO_CARD_URL3: 'https://github.com/MoChengqian',
-  //HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  HEO_INFO_CARD_URL3: 'https://github.com/MoChengqian',
+  HEO_INFO_CARD_TEXT3: 'GitHub',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -116,11 +112,8 @@ const CONFIG = {
     }
   ],
 
-  /*HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',*/
+  // 不显示右侧社群广告卡片
+  HEO_SOCIAL_CARD: false,
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
