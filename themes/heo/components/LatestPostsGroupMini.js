f0e6f30553ec6a3dc538976a8096da1e84bfcd68
MoChengqian <2972013548@qq.com>
2026-03-25 11:45:47 +0800

    style(heo): remove last edited time display

--- themes/heo/components/LatestPostsGroupMini.js
+++ themes/heo/components/LatestPostsGroupMini.js
@@ -51,7 +51,6 @@ export default function LatestPostsGroupMini({ latestPosts = [], siteInfo }) {
                   : 'text-slate-700 hover:text-blue-600'
               }`}>
               <div className='line-clamp-2 leading-5'>{post.title}</div>
-              <div className='mt-1 text-xs text-slate-400'>{post.lastEditedDay}</div>
             </div>
           </SmartLink>
         )
